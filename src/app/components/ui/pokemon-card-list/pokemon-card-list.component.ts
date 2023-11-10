25619b7d9bd2e16d7d6630e39f6c1c618f0abef5
Leandro Blodorn <leandro.blodorn@i2t-sa.com.ar>
2023-11-10 14:57:54 -0300

    Added some pokemon info to the description

--- src/app/components/ui/pokemon-card-list/pokemon-card-list.component.ts
+++ src/app/components/ui/pokemon-card-list/pokemon-card-list.component.ts
@@ -11,10 +11,15 @@ export class PokemonCardListComponent {
   @Input({ required: true }) items: Pokemon[] | null = null;
 
   map(item: Pokemon): Card {
+    const description: string =
+    `Id: ${item.id}`.concat('\n',
+    `Base experience: ${item.base_experience}`,'\n',
+    `Height: ${item.height}`);
+
     return {
       name: item.name,
       img: item.sprites.front_default,
-      description: 'none... for now...',
+      description,
     };
   }
 

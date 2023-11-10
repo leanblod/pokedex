34a23ca4b881a3798bc7a732ea679580636c2265
Leandro Blodorn <leandro.blodorn@i2t-sa.com.ar>
2023-11-10 11:54:38 -0300

    Forgot to remove stale code from AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { EnvService } from '@services/env.service';
-import { RequestData } from './components/request-form/request-form.component';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'poke-root',
@@ -14,26 +12,9 @@ export class AppComponent {
 
   constructor(
     private env: EnvService,
-    private http: HttpClient,
   ) {}
 
   public get production() {
     return this.env.production;
-  }
-
-  public send(data: RequestData) {
-    let fullUrl = `${data.url}/${data.endpoint}`;
-    if(data.id) {
-      fullUrl += `/${data.id}`;
-    }
-
-    this.http.get(fullUrl).subscribe({
-      next: (response) => {
-        this.response = response;
-      },
-      error(err) {
-        console.error(err);
-      },
-    });
   }
 }

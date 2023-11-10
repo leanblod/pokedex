99c47922c23ea22c4980daf1e6e61b883bdd7ee9
Leandro Blodorn <leandro.blodorn@i2t-sa.com.ar>
2023-11-10 11:26:11 -0300

    Generic Card-List component

---
+++ src/app/components/ui/card-list/card-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { Card } from '../card/card.component';
+
+@Component({
+  selector: 'poke-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.scss']
+})
+export class CardListComponent<Resource=unknown> {
+  /** List of objects to represent as cards */
+  @Input({ required: true }) items!: Resource[];
+
+  /**
+   * Helper function to map any resource as a card.
+   * If absent will try to cast the item as {@link Card}.
+   */
+  @Input() map: (item: Resource) => Card = (item) => item as Card;
+
+}

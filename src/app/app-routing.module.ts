4d4051259acc658da14e73b9cc2a042931918944
Leandro Blodorn <leandro.blodorn@i2t-sa.com.ar>
2023-11-10 11:30:40 -0300

    Router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokeApiEndpoint } from '@models/poke-api';
+import { Pokemon } from '@models/poke-api-resources/pokemon';
+import { PokeApiService } from '@services/poke-api.service';
+import { HomeComponent } from '@views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '' ,
+    pathMatch: 'full' ,
+    title: 'Home' ,
+    component: HomeComponent ,
+    resolve: {
+      pokemon: () => inject(PokeApiService).getListOf<Pokemon>(PokeApiEndpoint.Pokemon),
+    }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

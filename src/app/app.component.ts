fb712dffb7547d9507bfaf89eabf1d4c5ca35270
Leandro Blodorn <leandro.blodorn@i2t-sa.com.ar>
2023-11-10 11:33:03 -0300

    Implementing Router and adding declarations, imports and providers to AppModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { EnvService } from '@services/env.service';
+import { RequestData } from './components/request-form/request-form.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'poke-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pokedex';
+
+  private response: unknown;
+
+  constructor(
+    private env: EnvService,
+    private http: HttpClient,
+  ) {}
+
+  public get production() {
+    return this.env.production;
+  }
+
+  public send(data: RequestData) {
+    let fullUrl = `${data.url}/${data.endpoint}`;
+    if(data.id) {
+      fullUrl += `/${data.id}`;
+    }
+
+    this.http.get(fullUrl).subscribe({
+      next: (response) => {
+        this.response = response;
+      },
+      error(err) {
+        console.error(err);
+      },
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { httpInterceptorProviders } from '@interceptors/index';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CardComponent } from './components/ui/card/card.component';
+import { CardListComponent } from '@components/ui/card-list/card-list.component';
+import { HomeComponent } from '@views/home/home.component';
+import { EnvService } from '@services/env.service';
+import { PokeApiService } from '@services/poke-api.service';
+import { PokemonCardListComponent } from './components/ui/pokemon-card-list/pokemon-card-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardListComponent,
+    HomeComponent,
+    PokemonCardListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    httpInterceptorProviders,
+    EnvService,
+    PokeApiService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
